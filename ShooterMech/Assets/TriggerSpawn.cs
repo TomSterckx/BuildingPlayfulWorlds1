2d4bc3ca8f5d6cf335f1a031a2546249671f49f9
TomSterckx <36413442+TomSterckx@users.noreply.github.com>
2018-03-19 12:00:53 +0100

    Menu's

--- ShooterMech/Assets/TriggerSpawn.cs
+++ ShooterMech/Assets/TriggerSpawn.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class TriggerSpawn : MonoBehaviour
 {
+    public float numEnemies = 5;
+    public GameObject Enemyprefab;
+    public Vector3 center;
+    public Vector3 size;
+
 
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("TRIGGERED");
+        for (int i=0; i< numEnemies; i++)
+        {
+            Vector3 pos = center + new Vector3(Random.Range(-size.x/2, size.x/2), 0, Random.Range(-size.z / 2, size.z / 2));
+            Instantiate(Enemyprefab, pos, Quaternion.identity);
+        }
+
         Destroy(gameObject);
     }
 }
