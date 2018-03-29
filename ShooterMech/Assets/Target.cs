c353dc20190c182dbebf4ed28a90af7bde5106f5
TomSterckx <36413442+TomSterckx@users.noreply.github.com>
2018-03-29 11:48:00 +0200

    adj

--- ShooterMech/Assets/Target.cs
+++ ShooterMech/Assets/Target.cs
@@ -3,7 +3,8 @@
 public class Target : MonoBehaviour {
 
     public float health = 50f;
-   
+    public GameObject explosionEffect;
+
     public void TakeDamage(float amount)
     {
         health -= amount;
@@ -19,8 +20,10 @@ public class Target : MonoBehaviour {
             FindObjectOfType<GameManager>().amountBalls++;
         }
         Destroy(gameObject);
+        Instantiate(explosionEffect, transform.position, transform.rotation);
 
-        
+
+
 
     }
 }
